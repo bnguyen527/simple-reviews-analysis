a645bf66d5d76df6adde326b77fdd8dbd4b34771
Binh Nguyen <tug21976@temple.edu>
2019-04-05 21:52:17 -0400

    Implement train_nb without alpha hyperparameter

--- hw6.py
+++ hw6.py
@@ -1,3 +1,7 @@
+import numpy as np
+import pandas as pd
+
+
 def read_corpus(corpus_file):
     out = []
     with open(corpus_file, encoding='utf8') as f:
@@ -7,9 +11,45 @@ def read_corpus(corpus_file):
         return out
 
 
+def train_nb(training_data):
+    num_neg = 0
+    vocab = []
+    counts_neg = []
+    counts_pos = []
+    num_tokens_neg = 0
+    num_tokens_pos = 0
+    for label, tokens in training_data:
+        if label == 'neg':
+            num_neg += 1
+            num_tokens_neg += len(tokens)
+        else:
+            num_tokens_pos += len(tokens)
+        for token in tokens:
+            if token in vocab:
+                idx = vocab.index(token)
+            else:
+                idx = len(vocab)
+                vocab.append(token)
+                counts_neg.append(0)
+                counts_pos.append(0)
+            if label == 'neg':
+                counts_neg[idx] += 1
+            else:
+                counts_pos[idx] += 1
+    probs_neg = list(map(lambda c: c / num_tokens_neg, counts_neg))
+    probs_pos = list(map(lambda c: c / num_tokens_pos, counts_pos))
+    df_probs = pd.DataFrame(index=vocab)
+    df_probs['neg'] = probs_neg
+    df_probs['pos'] = probs_pos
+    return (num_neg / len(training_data), df_probs)
+
+
 def main():
     labeled_corpus = read_corpus('all_sentiment_shuffled.txt')
-    print(len(labeled_corpus))
+    split = round(len(labeled_corpus) * 0.8)
+    training_data = labeled_corpus[:split]
+    testing_data = labeled_corpus[split:]
+    prob_neg, df_probs = train_nb(training_data)
 
 
 if __name__ == "__main__":
